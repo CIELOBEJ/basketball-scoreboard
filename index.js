28addd8c5c06bb53d31ea40d0ad28808cead48f5
CIELOBEJ <cielobej77@gmail.com>
2023-12-18 18:18:12 +0100

    Commit initial file

---
+++ index.js
@@ -0,0 +1,36 @@
+let homePoints = document.getElementById("home-points")
+let homePointsEl = 0
+
+function add1Home() {
+    homePointsEl += 1
+    homePoints.textContent = homePointsEl
+    
+} 
+function add2Home() {
+    homePointsEl += 2
+    homePoints.textContent = homePointsEl
+    
+} 
+function add3Home() {
+    homePointsEl += 3
+    homePoints.textContent = homePointsEl
+    
+}
+let guestPoints = document.getElementById("guest-points")
+let guestPointsEl = 0
+
+function add1Guest() {
+    guestPointsEl += 1
+    guestPoints.textContent = guestPointsEl
+    
+} 
+function add2Guest() {
+    guestPointsEl += 2
+    guestPoints.textContent = guestPointsEl
+    
+} 
+function add3Guest() {
+    guestPointsEl += 3
+    guestPoints.textContent = guestPointsEl
+    
+}
